2b3ce9d14a780ca8f404aacf020b73ab10595a57
Eugenio Salas <eugeniosalasi@Eugenios-MacBook-Air-3.local>
2026-01-31 14:23:22 -0600

    Increase size of Tec de Monterrey logo

--- src/components/SponsorsCarousel.tsx
+++ src/components/SponsorsCarousel.tsx
@@ -44,7 +44,7 @@ const SponsorsCarousel = () => {
                     <img
                       src={sponsor.logo}
                       alt={sponsor.name}
-                      className={`${sponsor.logo.includes('Ing.png') ? 'max-h-64' : 'max-h-32'} w-auto object-contain`}
+                      className={`${sponsor.logo.includes('Ing.png') ? 'w-11/12 max-h-72' : 'max-h-32 w-auto'} object-contain`}
                     />
                   </div>
                   <p className="font-mono font-bold text-sm text-foreground text-center">
